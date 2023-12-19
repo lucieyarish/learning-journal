c45ccd557e858be3be403c5863012037bac941d2
Lucie Yarish <lucieyarish@hotmail.com>
2023-12-19 07:49:22 +0100

    Create data file for all articles; add all article images

---
+++ js/data.js
@@ -0,0 +1,75 @@
+export const articles = [
+  {
+    uuid: 'e36a8f90-b7cb-43e2-a609-0a6a466ecbc9',
+    date: 'July 23, 2023',
+    title: 'My new journey as a bootcamp student',
+    content: `After several months of learning in the Frontend Developer Career Path, 
+        I've made the big jump over to the Bootcamp to get expert code reviews of my 
+        Solo Projects projects and meet like-minded peers.`,
+    image: 'images/hero-post.jpg',
+  },
+  {
+    uuid: '9247c01b-4ac8-4193-aaeb-312628ff8e94',
+    date: 'May 10, 2023',
+    title: 'Blog one',
+    content: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+    image: 'images/article-image-01.jpg',
+  },
+  {
+    uuid: '7730bb18-5e95-4f14-a16e-b0f202bf44da',
+    date: 'May 18, 2023',
+    title: 'Blog two',
+    content: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+    image: 'images/article-image-02.jpg',
+  },
+  {
+    uuid: '94c3c2ef-f4aa-4ddb-8706-58bf7f01507d',
+    date: 'May 20, 2023',
+    title: 'Blog three',
+    content: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+    image: 'images/article-image-03.jpg',
+  },
+  {
+    uuid: 'f9b02684-35b8-49db-951b-81817c597a4c',
+    date: 'May 26, 2023',
+    title: 'Blog four',
+    content: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+    image: 'images/article-image-04.jpg',
+  },
+  {
+    uuid: '41b481d2-c000-4e22-8722-3d21ed944df3',
+    date: 'May 29, 2023',
+    title: 'Blog five',
+    content: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+    image: 'images/article-image-05.jpg',
+  },
+  {
+    uuid: 'fcf7e0ef-4293-4687-9637-6995071564aa',
+    date: 'June 1, 2023',
+    title: 'Blog six',
+    content: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+    image: 'images/article-image-06.jpg',
+  },
+  {
+    uuid: '7b8e38f6-4f18-42f0-a94b-7921bd470a3d',
+    date: 'July 3, 2023',
+    title: 'Blog seven',
+    content: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+    image: 'images/article-image-07.jpg',
+  },
+  {
+    uuid: '8c80f8ef-7c31-4b3d-8ce2-543b0cf2e931',
+    date: 'July 17, 2023',
+    title: 'Blog eight',
+    content: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! 
+    After several months of learning in the Frontend Developer Career Path.`,
+    image: 'images/article-image-08.jpg',
+  },
+];
