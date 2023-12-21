5823c7534eba43d1ed745debfb0fc892db42d25d
Lucie Yarish <lucieyarish@hotmail.com>
2023-12-21 22:09:05 +0100

    Add additional content to hero article; style article-detail page; make footer always stick to bottom

--- js/article-detail.js
+++ js/article-detail.js
@@ -3,29 +3,47 @@ import { formatDate } from './utils.js';
 
 const mainContainer = document.getElementById('main-container');
 
-const renderArticle = () => {
-  //TODO: Extract logic into new func
+const getSlugFromUrl = () => {
   const url = window.location.hash;
-  const slug = url.slice(1);
+  return url.slice(1);
+};
 
+const renderAdditionalContent = (article) => {
+  const html = `
+        <h3 class="subheading">${article.subheading1}</h3>
+        <p class="content-paragraph">${article.contentParagraph1}</p>
+        <p class="content-paragraph">${article.contentParagraph2}</p>
+        <h3 class="subheading">${article.subheading2}</h3>
+        <p class="content-paragraph">${article.contentParagraph3}</p>
+        <p class="content-paragraph">${article.contentParagraph4}</p>
+    `;
+  return html;
+};
+
+const renderArticle = () => {
+  const slug = getSlugFromUrl();
   const article = articles.find((a) => a.slug === slug);
 
+  //TODO: display full month name
   const date = formatDate(article.date);
   const html = `
-        <article data-id=${article.uuid} class="hero-post">
-            <img 
-                class="article-img hidden" 
-                src="../${article.image}" 
-                alt="${article.altText}">
+        <article id="article-container" data-id=${article.uuid} class="article-container">
             <p class="post-date">${date}</p>
-            <h1 class="hero-post-title">${article.title}</h1>
-            <p>
-                ${article.content}
-            </p>
+            <h1 class="post-title">${article.title}</h1>
+            <p>${article.content}</p>
+                <img 
+                    class="article-img hidden" 
+                    src="../${article.image}" 
+                    alt="${article.altText}">
         </article>
     `;
 
   mainContainer.innerHTML = html;
+
+  if (article.uuid === 'e36a8f90-b7cb-43e2-a609-0a6a466ecbc9') {
+    const currentArticle = document.getElementById('article-container');
+    currentArticle.innerHTML += renderAdditionalContent(article);
+  }
 };
 
 renderArticle();
--- js/data.js
+++ js/data.js
@@ -10,6 +10,19 @@ export const articles = [
     image: 'images/hero-post.jpg',
     altText:
       'A man wearing headphones sitting in the dark at his desk with three screens on it',
+    subheading1: 'How I stay committed to learning',
+    contentParagraph1: `I like to think of myself as a lifelong learner. 
+    I used to spend hours and hours learning, then try to create simple 
+    projects using what I learned or work new techniques into existing projects.`,
+    contentParagraph2: `While that was fun, I felt like it would be helpful to share what 
+    I was learning and most things about my journey with the world.`,
+    subheading2: 'How I got started',
+    contentParagraph3: `I started simple and gradually grew my learning journal site. 
+    I would take notes about what I was learning. After each learning session, 
+    I'd use my notes to not only reflect on what I learned but also write short summaries 
+    of what I learned using my own words.`,
+    contentParagraph4: `That helped me grok what I was learning, and I realized that posting 
+    my learning summaries was also helping others learn and stay motivated.`,
   },
   {
     uuid: '9247c01b-4ac8-4193-aaeb-312628ff8e94',
